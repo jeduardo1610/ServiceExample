cde82f5594abd959ca31f4836c7a43f0e76ecd74
Jorge Perez <jeduardo1601@gmail.com>
2016-01-28 23:41:47 -0500

    Simple Service Example

---
+++ app/src/main/java/com/example/m14x/serviceexample/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.m14x.serviceexample;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //starting service
+        findViewById(R.id.start_service).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ServiceCreate.class);
+                startService(intent);
+            }
+        });
+
+
+        //service onDestroy callback method will be called
+        findViewById(R.id.stop_Service).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ServiceCreate.class);
+                stopService(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/m14x/serviceexample/ServiceCreate.java
@@ -0,0 +1,73 @@
+package com.example.m14x.serviceexample;
+
+/**
+ * Created by m14x on 28/01/2016.
+ */
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+
+public class ServiceCreate extends Service {
+    private static final String TAG = "HelloService";
+
+    private boolean isRunning  = false;
+
+    @Override
+    public void onCreate() {
+        Log.i(TAG, "Service onCreate");
+
+        isRunning = true;
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+
+        Log.i(TAG, "Service onStartCommand");
+
+        //Creating new thread for my service
+        //Always write your long running tasks in a separate thread, to avoid ANR
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                //Your logic that service will perform will be placed here
+                //In this example we are just looping and waits for 1000 milliseconds in each loop.
+                for (int i = 0; i < 100; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+                    }
+
+                    if(isRunning){
+                        Log.i(TAG, "Service running");
+                    }
+                }
+
+                //Stop service once it finishes its task
+                stopSelf();
+            }
+        }).start();
+
+        return Service.START_STICKY;
+    }
+
+
+    @Override
+    public IBinder onBind(Intent arg0) {
+        Log.i(TAG, "Service onBind");
+        return null;
+    }
+
+    @Override
+    public void onDestroy() {
+
+        isRunning = false;
+
+        Log.i(TAG, "Service onDestroy");
+    }
+
+}
